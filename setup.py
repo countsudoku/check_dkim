0b92c1d05d4455f95991ef52b16900cf8f5b90a2
Moritz Schneider <countsudoku@users.noreply.github.com>
2018-05-04 15:06:28 +0200

    Move the requirement packages to a extra file

--- setup.py
+++ setup.py
@@ -5,13 +5,16 @@ from setuptools import setup, find_packages
 
 from check_dkim import __version__
 
+with open('requirements.txt', 'rb') as f:
+    requirements = [ package.decode().strip('\n') for package in f.readlines() if package ]
+
 setup(
     packages=find_packages(),
     name='check_dkim',
     version=__version__,
     author='Moritz C. K. U. Schneider',
     description='Nagios monitoring script to check DKIM DNS records.',
-    install_requires=['dnspython'],
+    install_requires=requirements,
     url='https://github.com/countsudoku/check_dkim',
     entry_points={
         'console_scripts': [
