abb9a16591d797c58eaee05ca72d238364e33f95
Moritz Schneider <countsudoku@users.noreply.github.com>
2018-05-08 14:31:20 +0200

    Add some documentaion

--- check_dkim/Nagios_output.py
+++ check_dkim/Nagios_output.py
@@ -4,11 +4,26 @@
 from . import Severity
 
 class Nagios_output(object):
+    """ writes a nagios compatible output
+
+    Args:
+        name (str): a general prefix for the output
+        writer (function): a function to write the result string
+    """
     def __init__(self, name='', writer=print):
         self.name = name
         self.writer = writer
 
     def write(self, severity, msg):
+        """ writes the msg
+
+        Args:
+            severity (str, int): the severity of the output
+            msg (str): the message to write
+
+        Returns:
+            int: the return code for nagios
+        """
         s = Severity(severity)
         output = '{name} {severity} - {msg}'.format(
             name=self.name,
