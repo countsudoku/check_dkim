b5603ef92c97ccb1367625c7901e11eb42b9c9d1
Moritz Schneider <countsudoku@users.noreply.github.com>
2018-05-08 11:49:10 +0200

    parse the version from __init__.py in setup.py , increased version

--- setup.py
+++ setup.py
@@ -1,12 +1,33 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import ast
+import os.path
+
 from setuptools import setup, find_packages
 
-from check_dkim import __version__
+def get_version(filepath, name):
+    """ get version from another python file without importing it
+
+    Args:
+        filepath (str): the file with the version
+        name (str): the variable name of the version string
+
+    Returns:
+        the version string
+    """
+    with open(filepath, 'rb') as f:
+        version_code = next(
+            line for line in f.readlines() if line.strip().startswith(name.encode()))
+    return ast.parse(version_code).body[0].value.s
 
 with open('requirements.txt', 'rb') as f:
     requirements = [ package.decode().strip('\n') for package in f.readlines() if package ]
+
+__version__ = get_version(
+    os.path.join(os.path.dirname(__file__), 'check_dkim/__init__.py'),
+    '__version__'
+    )
 
 setup(
     packages=find_packages(),
